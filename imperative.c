26998a1ee22390d50bf21b5e6db30d22be9ca534
Peter <plarochkin@gmail.com>
2022-12-18 00:19:13 +0300

    new

--- imperative.c
+++ imperative.c
@@ -174,87 +174,98 @@ void applyA_to_omega() {
     }
 }
 
-// void applyA_to_r() {
-//     size_t i, j;
-//     #pragma dvm region
-//     {
-//         //with padding, inside "picture"
-//         #pragma dvm parallel([i][j] on A_r[i][j]) shadow_renew(r)
-//         for (i = 1; i < M; ++i) {
-//             for (j = 1; j < N; ++j) {
-//                 // here is (7) equation works
-//                 A_r[i][j] = r[i][j] * (2/(h1*h1) + 2/(h2*h2) + q(A1+ i*h1, B1+ j*h2)) + 
-//                                     r[i-1][j] * (-1/(h1*h1)) +
-//                                     r[i+1][j] * (-1/(h1*h1)) +
-//                                     r[i][j-1] * (-1/(h2*h2)) +
-//                                     r[i][j+1] * (-1/(h2*h2));
-//             }
-//         }
+void applyA_to_r() {
+    size_t i, j;
+    #pragma dvm region
+    {
+        //with padding, inside "picture"
+        #pragma dvm parallel([i][j] on A_r[i][j]) shadow_renew(r)
+        for (i = 1; i < M; ++i) {
+            for (j = 1; j < N; ++j) {
+                // here is (7) equation works
+                A_r[i][j] = r[i][j] * (2/(h1*h1) + 2/(h2*h2) + q(A1+ i*h1, B1+ j*h2)) + 
+                                    r[i-1][j] * (-1/(h1*h1)) +
+                                    r[i+1][j] * (-1/(h1*h1)) +
+                                    r[i][j-1] * (-1/(h2*h2)) +
+                                    r[i][j+1] * (-1/(h2*h2));
+            }
+        }
 
-//         #pragma dvm parallel([i] on A_r[i][0]) shadow_renew(r)
-//         for (i = 1; i < M; ++i) {
-//             // it's (10) equations
-//             // i=1,M-1
-//             // bottom applying
-//             A_r[i][0] = -2/(h2*h2) * (r[i][1] - r[i][0]) +
-//                                 ( q(A1+ i*h1,B1+ 0*h2) + 2/h2 ) * r[i][0] -
-//                                 1/(h1*h1)*(r[i+1][0] - r[i][0] - r[i][0]+ r[i-1][0]);
-//         }
+        #pragma dvm parallel([i] on A_r[i][0]) shadow_renew(r)
+        for (i = 0; i < M; ++i) {
+            if (i >= 1) {
+                // it's (10) equations
+                // i=1,M-1
+                // bottom applying
+                A_r[i][0] = -2/(h2*h2) * (r[i][1] - r[i][0]) +
+                                ( q(A1+ i*h1,B1+ 0*h2) + 2/h2 ) * r[i][0] -
+                                1/(h1*h1)*(r[i+1][0] - r[i][0] - r[i][0]+ r[i-1][0]);
+            } else if (i == 0){  
+                // remaining corner points
+                // bottom left
+                // it's (11) equation
+                A_r[0][0] = -2/(h1*h1)*(r[1][0] - r[0][0]) - 
+                            2/(h2*h2)*(r[0][1] - r[0][0]) +
+                            (q(A1+ 0*h1,B1+ 0*h2) + 2/h1 + 2/h2) * r[0][0];
+            }
+        }
 
-//         #pragma dvm parallel([i] on A_r[i][N]) shadow_renew(r)
-//         for (i = 1; i < M; ++i) {
-//             // it's (10) equations
-//             // i=1,M-1
-//             // top applying
-//             A_r[i][N] = 2/(h2*h2) * (r[i][N] - r[i][N-1]) +
-//                                 ( q(A1+ i*h1,B1+ N*h2) + 2/h2 ) * r[i][N] -
-//                                 1/(h1*h1)*(r[i+1][N] - r[i][N] - r[i][N]+ r[i-1][N]); 
-//         }
+        #pragma dvm parallel([i] on A_r[i][N]) shadow_renew(r)
+        for (i = 1; i <= M; ++i) {
+            if (i < M){
+            // it's (10) equations
+            // i=1,M-1
+            // top applying
+            A_r[i][N] = 2/(h2*h2) * (r[i][N] - r[i][N-1]) +
+                                ( q(A1+ i*h1,B1+ N*h2) + 2/h2 ) * r[i][N] -
+                                1/(h1*h1)*(r[i+1][N] - r[i][N] - r[i][N]+ r[i-1][N]);
+            } else if (i == M) {
+      
+            // it's (13) equation
+            // top right
+            A_r[M][N] = 2/(h1*h1)*(r[M][N] - r[M-1][N]) +
+                            2/(h2*h2)*(r[M][N] - r[M][N-1]) +
+                            (q(A1+ M*h1,B1+ N*h2) + 2/h1 + 2/h2) * r[M][N]; 
+            }
+        }
 
-//         #pragma dvm parallel([j] on A_r[0][j]) shadow_renew(r)
-//         for (j = 1; j < N; ++j) {
-//             // it's (9) equations
-//             // j=1,N-1
-//             // left applying
-//             A_r[0][j] = -2/(h1*h1) * (r[1][j] - r[0][j]) + 
-//                                 (q(A1+ 0*h1,B1+ j*h2) + 2/h1) * r[0][j] - 
-//                                 1/(h2*h2)*(r[0][j+1] - r[0][j] - r[0][j]+ r[0][j-1]);
-//         }
+        #pragma dvm parallel([j] on A_r[0][j]) shadow_renew(r)
+        for (j = 1; j <= N; ++j) {
+            if (j < N) {
+                // it's (9) equations
+                // j=1,N-1
+                // left applying
+                A_r[0][j] = -2/(h1*h1) * (r[1][j] - r[0][j]) + 
+                                    (q(A1+ 0*h1,B1+ j*h2) + 2/h1) * r[0][j] - 
+                                    1/(h2*h2)*(r[0][j+1] - r[0][j] - r[0][j]+ r[0][j-1]);
+            } else if (j == N){
+                // it's (14) equation
+                // top left
+                A_r[0][N] = -2/(h1*h1)*(r[1][N]- r[0][N])+
+                                2/(h2*h2)*(r[0][N]- r[0][N-1])+
+                                (q(A1+ 0*h1,B1+ N*h2) + 2/h1 + 2/h2) * r[0][N];
+            }
+        }
 
-//         #pragma dvm parallel([j] on A_r[M][j]) shadow_renew(r)
-//         for (j = 1; j < N; ++j) {
-//             // it's (9) equations
-//             // j=1,N-1
-//             // right applying
-//             A_r[M][j] = 2/(h1*h1) * (r[M][j] - r[M-1][j]) + 
-//                                 (q(A1+ M*h1,B1+ j*h2) + 2/h1) * r[M][j] - 
-//                                 1/(h2*h2)*(r[M][j+1] - r[M][j] - r[M][j]+ r[M][j-1]);
-//         }
-
-//         // remaining corner points
-//         // bottom left
-//         // it's (11) equation
-//         A_r[0][0] = -2/(h1*h1)*(r[1][0] - r[0][0]) - 
-//                             2/(h2*h2)*(r[0][1] - r[0][0]) +
-//                             (q(A1+ 0*h1,B1+ 0*h2) + 2/h1 + 2/h2) * r[0][0];
-
-//         // it's (12) equation
-//         // bottom right
-//         A_r[M][0] = 2/(h1*h1)*(r[M][0] - r[M-1][0]) - 
-//                             2/(h2*h2)*(r[M][1] - r[M][0]) +
-//                             (q(A1+ M*h1,B1+ 0*h2) + 2/h1 + 2/h2) * r[M][0];
-//         // it's (13) equation
-//         // top right
-//         A_r[M][N] = 2/(h1*h1)*(r[M][N] - r[M-1][N]) +
-//                             2/(h2*h2)*(r[M][N] - r[M][N-1]) +
-//                             (q(A1+ M*h1,B1+ N*h2) + 2/h1 + 2/h2) * r[M][N];
-//         // it's (14) equation
-//         // top left
-//         A_r[0][N] = -2/(h1*h1)*(r[1][N]- r[0][N])+
-//                             2/(h2*h2)*(r[0][N]- r[0][N-1])+
-//                             (q(A1+ 0*h1,B1+ N*h2) + 2/h1 + 2/h2) * r[0][N];
-//     }
-// }
+        #pragma dvm parallel([j] on A_r[M][j]) shadow_renew(r)
+        for (j = 0; j < N; ++j) {
+            if (j >=  1) {
+                // it's (9) equations
+                // j=1,N-1
+                // right applying
+                A_r[M][j] = 2/(h1*h1) * (r[M][j] - r[M-1][j]) + 
+                                    (q(A1+ M*h1,B1+ j*h2) + 2/h1) * r[M][j] - 
+                                    1/(h2*h2)*(r[M][j+1] - r[M][j] - r[M][j]+ r[M][j-1]);
+            } else if (j == 0) {
+                // it's (12) equation
+                // bottom right
+                A_r[M][0] = 2/(h1*h1)*(r[M][0] - r[M-1][0]) - 
+                                2/(h2*h2)*(r[M][1] - r[M][0]) +
+                                (q(A1+ M*h1,B1+ 0*h2) + 2/h1 + 2/h2) * r[M][0];
+            }
+        }
+    }
+}
 
 // void getB() {
 //     size_t i, j;
