cec2e1ca31b331af4586617519953035b719b457
Peter <plarochkin@gmail.com>
2022-12-21 14:11:03 +0300

    new

--- imperative.c
+++ imperative.c
@@ -432,7 +432,7 @@ int main(/*int argc, char** argv*/) {
     getB();
 
     int count = 0;
-    while (squared_difference >= sq_eps && count < 500)
+    while (squared_difference >= sq_eps && count < 2500)
     {
         size_t i, j;
         #pragma dvm region
