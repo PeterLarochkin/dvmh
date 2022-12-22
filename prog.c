ef03ff15e862e6907d028075b152e1e714a83fea
Peter <plarochkin@gmail.com>
2022-12-22 18:47:57 +0300

    new

--- prog.c
+++ prog.c
@@ -160,7 +160,7 @@ int main() {
     }
 
     int count = 0;
-    while (squared_difference >= sq_eps && count < 1)
+    while (squared_difference >= sq_eps && count < 10)
     {
         #pragma dvm region
         {
